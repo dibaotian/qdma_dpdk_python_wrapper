965dcd2dc3761b9b887f03783ba625fdf5a8c582
laba22@163.com <root@xshdcg03.xlnx.xilinx.com>
2020-06-18 13:34:21 +0800

    update

--- README.md
+++ README.md
@@ -1,14 +1,28 @@
 # qdma_dpdk_python_wrapper
 
 
-1 he project is based on the xilinx QDMA  (2019.1)
+1 The Plugin is based on the xilinx QDMA (2019.1)
   https://github.com/Xilinx/dma_ip_drivers
-2 The project is based on the python2.7
+2 The Plugin is based on the python2.7
+3 The Plugin is based on the DPDK dpdk-stable-18.11.5
+4 Support Alveo card U50
 
-3 Releated document
+5 Related document
   https://xilinx.github.io/dma_ip_drivers/2019.1/DPDK/html/index.html
-4 support U50/U200 etc Alveo card
+  https://www.xilinx.com/support/documentation/ip_documentation/qdma/v3_0/pg302-qdma.pdf
+  https://www.xilinx.com/support/answers/71453.html
+
+### Prerequisites
+ CentOS 7.4 1708 3.10.0-862.el7.x86_64
+ GCC 4.8.5 20150623 (Red Hat 4.8.5-36) (GCC)
+ Xilinx QDMA IP (Vivado 2018.3)
+ Xilinx QDMA DPDK driver (2019.1)
+ DPDK  dpdk-stable-18.11.5
+ Alveo card U50
+ Python 2.7
+ 
 
 
-bug list:
-REG DUMP will cause host crash
+
+###Known issues
+reg dump will cause host crash
--- dpdk.py
+++ dpdk.py
@@ -1,7 +1,8 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
 import os
 import sys
-
-import subprocess
 import pexpect
 
 #basedir = os.path.dirname(__file__)
@@ -43,39 +44,49 @@ def send_cmd(self):
 
 class Qdmaplugin():
     def __init__(self, cmd):
+        """
+        Start the qdma app
+        :param cmd:
+        cmd represents qdma comand string
+        """
         self.cmd = cmd
 
-        print ("#######################################")
-        print ("###########start the server#############")
-        print ("#######################################")
+        print ("##################################################################")
+        print ("###########start the Xilinx QDMA DPDK plugin service##############")
+        print ("##################################################################")
+
+        # todo parameter check
 
         # self.process = pexpect.spawn(self.cmd, timeout=None, logfile=sys.stdout)
         self.process = pexpect.spawn(self.cmd, timeout=None)
 
     def flush_output(self):
+        """
+        Clear the contents of readbufer
+        :return:
+        """
         self.process.expect("xilinx-app>")
 
     def port_init(self, port_id, num_queues, st_queues, ring_depth, pkt_buffer_size):
-        """port initailization
-        port_init < port - id > < num - queues > < st - queues > < ring - depth > < pkt - buff - size >
-
-        This command assigns queues to the port, sets up required resources for the queues,
-        and prepares queues for data processing
+        """
+        Assigns queues to the port, sets up required resources for the queues and prepares queues for data processing
+        :param port_id:
+        :param num_queues:
+        :param st_queues:
+        :param ring_depth:
+        :param pkt_buffer_size:
+        :return: string of init result
 
         port-id represents a logical numbering for PCIe functions in the order they are bind to igb_uio driver.
                 The first PCIe function that is bound has port id as 0.
-
         num-queues represents the total number of queues to be assigned to the port
-
         num-st-queues represents the total number of queues to be configured in streaming mode.
                       This implies that the (num-queues - num-st-queues) number of queues has to be configured
                       in memory mapped mode.
-
         ring-depth represents the number of entries in C2H and H2C descriptor rings of each queue of the port
-
         pkt-buff-size represents the size of the data that a single C2H or H2C descriptor can support
-
-        example  port_init 0 1 1 1024 2048"""
+        example  port_init 0 1 1 1024 2048
+        """
 
         port_cmd = 'port_init %s %s %s %s %s' % (port_id, num_queues, st_queues, ring_depth, pkt_buffer_size)
 
@@ -87,11 +98,14 @@ class Qdmaplugin():
         # print self.process.after
 
     def port_close(self, port_id):
-        """port close
-        port_close <port-id>
-        This command frees up all the allocated resources and removes the queues associated with the port
+        """
+        Free up all the allocated resources and removes the queues associated with the port.
+        :param port_id:
+        :return:string of close result
+
         port-id represents a logical numbering for PCIe functions in the order they are bind to igb_uio driver.
-        The first PCIe function that is bound has port id as 0"""
+                The first PCIe function that is bound has port id as 0
+        """
 
         port_cmd = 'port_close %s' % port_id
         self.process.sendline(port_cmd)
@@ -101,8 +115,24 @@ class Qdmaplugin():
         # self.process.logfile_read = sys.stdout
 
     def port_reset(self, port_id, num_queues, st_queues, ring_depth, pkt_buff_size):
-        """port reset
-        port_reset <port-id> <num-queues> <st-queues> <ring-depth > <pkt-buff-size>"""
+        """
+        Reset specified port
+        :param port_id:
+        :param num_queues:
+        :param st_queues:
+        :param ring_depth:
+        :param pkt_buff_size:
+        :return: string of reset result
+
+        port-id represents a logical numbering for PCIe functions in the order they are bind to igb_uio driver.
+                The first PCIe function that is bound has port id as 0
+        num-queues represents the total number of queues to be assigned to the port
+        num-st-queues represents the total number of queues to be configured in streaming mode.
+                      This implies that the (num-queues - num-st-queues) number of queues has to be configured
+                      in memory mapped mode.
+        ring-depth represents the number of entries in C2H and H2C descriptor rings of each queue of the port
+        pkt-buff-size represents the size of the data that a single C2H or H2C descriptor can support
+        """
 
         port_cmd = 'port_reset %s %s %s %s %s' % (port_id, num_queues, st_queues, ring_depth, pkt_buff_size)
         self.process.sendline(port_cmd)
@@ -112,8 +142,14 @@ class Qdmaplugin():
         # self.process.logfile_read = sys.stdout
 
     def port_remove(self, port_id):
-        """port remove
-        port_remove <port-id>"""
+        """
+        Free up all the resources allocated for the port and removes the port from application use
+        :param port_id:
+        :return:
+
+        port-id represents a logical numbering for PCIe functions in the order they are bind to igb_uio driver.
+                The first PCIe function that is bound has port id as 0
+        """
 
         port_cmd = 'port_remove %s' % port_id
         self.process.sendline(port_cmd)
@@ -123,16 +159,20 @@ class Qdmaplugin():
         # self.process.logfile_read = sys.stdout
 
     def reg_read(self, port_id, bar_num, address):
-        """Reads Specified Register
-        reg_read <port-id> <bar-num> <address>
-        This command is used to read the specified register
+        """
+        Read specified register
+        :param port_id:
+        :param bar_num:
+        :param address:
+        :return: register value
 
         port-id represents a logical numbering for PCIe functions in the order they are bind to igb_uio driver.
-        The first PCIe function that is bound has port id as 0
+                The first PCIe function that is bound has port id as 0
 
         bar-num represents the PCIe BAR where the register is located
 
-        address represents offset of the register in the PCIe BAR bar-num"""
+        address represents offset of the register in the PCIe BAR bar-num
+        """
 
         reg_cmd = 'reg_read %s %s %s' % (port_id, bar_num, address)
         self.process.sendline(reg_cmd)
@@ -142,18 +182,20 @@ class Qdmaplugin():
         # self.process.logfile_read = sys.stdout
 
     def reg_write(self, port_id, bar_num, address, value):
-        """Writes Specified Register
-        reg_write <port-id> <bar-num> <address> <value>
-        This command is used to write a 32-bit value to the specified register
+        """
+        Write a 32-bit value to the specified register
+        :param port_id:
+        :param bar_num:
+        :param address:
+        :param value:
+        :return: reg value/succeed/fail
 
         port-id represents a logical numbering for PCIe functions in the order they are bind to igb_uio driver.
-        The first PCIe function that is bound has port id as 0.
-
+                The first PCIe function that is bound has port id as 0.
         bar-num represents the PCIe BAR where the register is located
-
         address represents offset of the register in the PCIe BAR bar-num
-
-        value represents the value to be written at the register offset address"""
+        value represents the value to be written at the register offset address
+        """
 
         reg_cmd = 'reg_write %s %s %s %s' % (port_id, bar_num, address, value)
         self.process.sendline(reg_cmd)
@@ -163,12 +205,14 @@ class Qdmaplugin():
         # self.process.logfile_read = sys.stdout
 
     def reg_dump(self, port_id):
-        """To dump all the valid registers
-        reg_dump <port-id>
-        This command dumps important QDMA registers values of the given port on console
+        """
+        Dump registers values of the given port
+        :param port_id:
+        :return: reg values
 
         port-id represents a logical numbering for PCIe functions in the order they are bind to igb_uio driver.
-        The first PCIe function that is bound has port id as 0."""
+                The first PCIe function that is bound has port id as 0
+        """
 
         # currently this method will cause host crash
 
@@ -178,11 +222,101 @@ class Qdmaplugin():
         result = self.process.before
         return result
 
-    def queue_dump (self, port_id, queue_id):
-        
+    def queue_dump(self, port_id, queue_id):
+        """
+        Dump the queue-context of a queue-number
+        :param port_id:
+        :param queue_id:
+        :return: queue-context
 
+        port-id represents a logical numbering for PCIe functions in the order they are bind to igb_uio driver.
+                The first PCIe function that is bound has port id as 0.
+        queue-id represents the queue number relative to the port, whose context information needs to be logged
+        """
 
-    def process_return (self):
+        dump_cmd = 'queue_dump %s %s' % (port_id, queue_id)
+        self.process.sendline(dump_cmd)
+        self.process.expect("xilinx-app>")
+        result = self.process.before
+        return result
+
+    def desc_dump(self, port_id, queue_id):
+        """
+        Dump the descriptors of the C2H and H2C rings of the specified queue number of the given port.
+        :param port_id:
+        :param queue_id:
+        :return: desctiptor
+
+        port-id represents a logical numbering for PCIe functions in the order they are bind to igb_uio driver.
+                The first PCIe function that is bound has port id as 0.
+        queue-id represents the queue number relative to the port, whose context information needs to be logged
+        """
+
+        dump_cmd = 'desc_dump %s %s' % (port_id, queue_id)
+        self.process.sendline(dump_cmd)
+        self.process.expect("xilinx-app>")
+        result = self.process.before
+        return result
+
+    def dma_to_device(self, port_id, num_queues, input_filename, dst_addr, size, iterations):
+        """
+        DMA the data from host to card
+        :param port_id:
+        :param num_queues:
+        :param input_filename:
+        :param dst_addr:
+        :param size:
+        :param iterations:
+        :return:
+
+        port-id represents a logical numbering for PCIe functions in the order they are bind to igb_uio driver
+                The first PCIe function that is bound has port id as 0
+        num-queues represents the total number of queues to use for transmitting the data
+        input-filename represents the path to a valid binary data file, contents of which needs to be DMA’ed
+        dst-addr represents the destination address (offset) in the card to where DMA should be done in memory mapped mode
+                 This field is ignored for streaming mode queues.
+        size represents the amount of data in bytes that needs to be transmitted to the card from the given input file
+             Data will be segmented across queues such that the total data transferred to the card is size amount
+        iterations represents the number of loops to repeat the same DMA transfer
+        """
+
+        dma_to_dev_cmd = 'dma_to_device %s %s %s %s %s %s' % (
+        port_id, num_queues, input_filename, dst_addr, size, iterations)
+        self.process.sendline(dma_to_dev_cmd)
+        self.process.expect("xilinx-app>")
+        result = self.process.before
+        return result
+
+    def dma_from_device(self, port_id, num_queues, output_filename, src_addr, size, iterations):
+        """
+        DMA the data from card to host
+        :param port_id:
+        :param num_queues:
+        :param output_filename:
+        :param src_addr:
+        :param size:
+        :param iterations:
+        :return:
+
+        port-id represents a logical numbering for PCIe functions in the order they are bind to igb_uio driver
+                The first PCIe function that is bound has port id as 0
+        num-queues represents the total number of queues to use for transmitting the data
+        output-filename represents the path to output file to dump the received data
+        src-addr represents the source address (offset) in the card from where DMA should be done in memory mapped mode.
+                 This field is ignored for streaming mode queues.
+        size represents the amount of data in bytes that needs to be received from the card.
+             Data will be segmented across queues such that the total data transferred from the card is size amount
+        iterations represents the number of loops to repeat the same DMA transfer
+        """
+
+        dma_from_dev_cmd = 'dma_to_device %s %s %s %s %s %s' % (
+        port_id, num_queues, output_filename, src_addr, size, iterations)
+        self.process.sendline(dma_from_dev_cmd)
+        self.process.expect("xilinx-app>")
+        result = self.process.before
+        return result
+
+    def process_return(self):
         self.process.kill(1)
         print "Exit qdma process"
 
@@ -200,18 +334,42 @@ if __name__ == "__main__":
     dpdkp.flush_output()
 
     ret = dpdkp.port_init(0, 1, 1, 1024, 2048)
+    # print ret
+
+    ret = dpdkp.dma_to_device(0, 1,'data/datafile0_4K.bin', 0, 4096, 1)
     print ret
 
-    ret = dpdkp.reg_read(0, 0, 0)
+    ret = dpdkp.dma_from_device(0, 1, 'data/port0_qcount0_size4k.bin', 0, 4096, 1)
     print ret
 
-    ret = dpdkp.reg_write(0, 0, 0, 0x1)
-    print ret
 
-    ret = dpdkp.reg_dump(0)
-    print ret
 
-    ret = dpdkp.port_reset(0, 1, 1, 1024, 2048)
+
+    # ret = dpdkp.reg_read(0, 0, 0)
+    # print ret
+
+    # ret = dpdkp.reg_write(0, 0, 0, 0x1)
+    # print ret
+
+    #  It will cause system crash
+    # ret = dpdkp.reg_dump(0)
+    # print ret
+
+    # ret = dpdkp.queue_dump(0, 0)
+    # print ret
+
+    # ret = dpdkp.desc_dump(0, 0)
+    # print ret
+
+    #  return failure
+    # ret = dpdkp.port_reset(0, 1, 1, 1024, 2048)
+    # print ret
+
+    #  It will cause system crash
+    # ret = dpdkp.port_remove(0)
+    # print ret
+
+    ret = dpdkp.port_close(0)
     print ret
 
 
